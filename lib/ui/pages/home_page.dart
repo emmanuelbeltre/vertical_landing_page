8f61abe4db666aff1eb06b0cab2d9574f7c7a9a9
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-08-26 17:44:18 -0400

    All views added

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/views/about_view.dart';
+import 'package:vertical_landing_page/ui/views/contact_view.dart';
 
-import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/views/home_view.dart';
+import 'package:vertical_landing_page/ui/views/location_view.dart';
+import 'package:vertical_landing_page/ui/views/pricing_view.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red[200],
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            _HomeBody(),
+            Positioned(
+                right: 20,
+                top: 20,
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  color: Colors.black,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView()
+      ],
     );
   }
 }
---
+++ lib/ui/views/location_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LocationView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.teal,
+      child: Center(
+        child: FittedBox(
+          child: Text(
+            'Location',
+            style: GoogleFonts.montserratAlternates(
+                fontSize: 80, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
